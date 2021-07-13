8aa8cda6a923b6a1233e5fb0aeba1c3067c6b967
tusharsrivastava2904 <tusharsrivastava2904@gmail.com>
2021-07-13 06:25:54 +0530

    split string token : Regex

---
+++ StingTokens.java
@@ -0,0 +1,28 @@
+/*
+ * Tushar Srivastava
+ * #100DaysOfCode
+ * tusharsrivastava2904@gmail.com 
+*/
+
+// split user input string into tokens for delimiters [!,?._'@\\s]+  -> ! , ? . _ ' @ space
+import java.util.*;
+
+public class StingTokens {
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        String s = scan.nextLine().trim();
+	        
+	        if (s.length() > 0) {
+		        String[] tokens = s.split("[!,?._'@\\s]+");
+		        System.out.println(tokens.length);
+		        for(String token : tokens) {
+		            System.out.println(token);
+	        }
+	        } else {
+	        	System.out.println("0");
+	        }
+	        
+	        scan.close();
+	    }
+}
