1d3e3ef6b89971d10761403998c8b1995be2a042
TUSHAR SRIVASTAVA <62189343+tusharsrivastava2904@users.noreply.github.com>
2021-11-14 00:50:11 +0530

    CIELRCPT chef

---
+++ ProblemSolving/CodeChef/CIELRCPT.java
@@ -0,0 +1,44 @@
+package ProblemSolving.CodeChef;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//problem code: CIELRCPT
+class CIELRCPT {
+    public static void main (String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+
+        int[] prices = new int[12];
+        for(int i=0; i<12; i++){
+            prices[i] = (int) Math.pow(2,i);
+        }
+
+        int[] target = new int[testCase];
+
+        for(int i=0; i<testCase; i++){
+            target[i]=sc.nextInt();
+        }
+
+        for(int i=0; i<target.length; i++){
+            findMin(prices, target[i]);
+        }
+    }
+
+    public static void findMin(int[] prices, int target){
+        int remP=target, count=0;
+        for(int i=prices.length-1; i>=0; i--){
+            while(remP>=0){
+                if(remP>=prices[i]){
+                    remP-=prices[i];
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
