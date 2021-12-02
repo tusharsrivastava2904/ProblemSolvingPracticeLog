6eb039dff1424914651233a89ba4a0bdf1b924dc
TUSHAR SRIVASTAVA <62189343+tusharsrivastava2904@users.noreply.github.com>
2021-12-02 13:59:13 +0530

    tcs conveyor belt problem

--- ProblemSolving/TCSChocolateFactory.java
+++ ProblemSolving/TCSChocolateFactory.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 //TCS problem: A chocolate factory is packing chocolates into the packets. The task is to move the empty packets at the end of the conveyor
 //for eg: [6,0,1,8,0,2] has two empty packets so after moving them to the end of the conveyor belt we get -> [6,1,8,2,0,0]
+//time: O(n)
 public class TCSChocolateFactory {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
