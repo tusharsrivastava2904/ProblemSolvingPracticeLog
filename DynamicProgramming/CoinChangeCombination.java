91f557d35f6b7c72a0de40cc55cbd4a0a3779e97
TUSHAR SRIVASTAVA <62189343+tusharsrivastava2904@users.noreply.github.com>
2022-02-04 23:48:58 +0530

    DP Package created

---
+++ DynamicProgramming/CoinChangeCombination.java
@@ -0,0 +1,26 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+/*
+* Question: Find the maximum no. of combinations that can be used to pay a certain value
+*           from each denomination of coins with infinite supplies
+*
+* Test Case:
+*           2   3   5   //coin
+*           7           //value
+*           ----------
+*           2
+*
+* Explanation:
+*           2 2 3
+*           2 5
+*           these are the only two ways to pay Rs. 7 from the given supplies of coins
+*/
+
+public class CoinChangeCombination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
